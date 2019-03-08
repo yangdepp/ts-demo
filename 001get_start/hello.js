870e5b1fc5d9568f85c44aea78c2cd5ffd09f981
yangdepu <yangdepu@zdsoft.cn>
2019-03-08 11:53:31 +0800

    类接口的示例

--- 001get_start/hello.js
+++ 001get_start/hello.js
@@ -1,5 +1,18 @@
+var Student = /** @class */ (function () {
+    //  public相当于定义了一个类的一个同名属性
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 function greeter(person) {
-  return "hello" + person;
+    return "Hello, " + person.firstName + " " + person.lastName;
 }
-var user = 'yangdepp';
-document.body.innerHTML = greeter(user);
+var user = new Student("yang", "J", "Deep");
+console.log(user); //  Student {firstName: "yang", middleInitial: "J", lastName: "Deep", fullName: "yang J Deep"}
+console.log(greeter(user));
+//  greeter函数传入的是一个Person的interface，他要求有两个属性（firstName，lastName）
+//  new 出来的Student实例，有四个属性（firstName、middleInitial、lastName、fullName），所以符合参数
--- 001get_start/hello.ts
+++ 001get_start/hello.ts
@@ -1,7 +1,23 @@
-function greeter(person) {
-  return "hello" + person
+class Student {
+  fullName: string;
+  //  public相当于定义了一个类的一个同名属性
+  constructor(public firstName, public middleInitial, public lastName) {
+    this.fullName = firstName + " " + middleInitial + " " + lastName
+  }
 }
 
-let user = 'yangdepp'
+interface Person {
+  firstName: string;
+  lastName: string;
+}
 
-document.body.innerHTML = greeter(user)
+function greeter(person: Person) {
+  return "Hello, " + person.firstName + " " + person.lastName;
+}
+
+let user = new Student("yang", "J", "Deep");
+console.log(user)  //  Student {firstName: "yang", middleInitial: "J", lastName: "Deep", fullName: "yang J Deep"}
+console.log(greeter(user))
+
+//  greeter函数传入的是一个Person的interface，他要求有两个属性（firstName，lastName）
+//  new 出来的Student实例，有四个属性（firstName、middleInitial、lastName、fullName），所以符合参数
